51ed358befadc2b395cb713f85726abcf1a608e2
Martin Kroll <mrtnkrll@gmail.com>
2020-11-30 17:58:31 +0300

    Changing DB engine to SQLite

--- arbcalc/settings.py
+++ arbcalc/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'arbcalc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'arbcalc',
-        'USER': 'postgres',
-        'PASSWORD': 'Slimahdy12',
-        'POST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'arbcalc',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Slimahdy12',
+#         'POST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
