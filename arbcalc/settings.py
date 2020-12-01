168180cec058b96760544bf2a40ffa106c14e83d
Martin Kroll <mrtnkrll@gmail.com>
2020-12-01 12:57:22 +0300

    added arbitrationfee.com

--- arbcalc/settings.py
+++ arbcalc/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['arbcalc.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['arbcalc.herokuapp.com', 'arbitrationfee.com', 'localhost']
 
 
 # Application definition
